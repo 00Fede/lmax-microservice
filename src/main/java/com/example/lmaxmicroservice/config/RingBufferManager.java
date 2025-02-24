547662746a27b340c0265bfbd49e27c6d4a6034b
Federico Ocampo <feoorti@bancolombia.com.co>
2025-02-24 15:01:19 -0500

    Refactor RingBufferManager to simplify Disruptor initialization by removing unnecessary ExecutorService

--- src/main/java/com/example/lmaxmicroservice/config/RingBufferManager.java
+++ src/main/java/com/example/lmaxmicroservice/config/RingBufferManager.java
@@ -2,15 +2,11 @@ package com.example.lmaxmicroservice.config;
 
 import org.springframework.context.annotation.Configuration;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 import org.springframework.context.annotation.Bean;
 import com.lmax.disruptor.RingBuffer;
 import com.lmax.disruptor.dsl.Disruptor;
 import com.example.lmaxmicroservice.event.LmaxEventFactory;
 import com.example.lmaxmicroservice.event.LmaxEventHandler;
-import com.example.lmaxmicroservice.event.LmaxEventHandlerOutput;
 import com.example.lmaxmicroservice.event.Event;
 import com.lmax.disruptor.SleepingWaitStrategy;
 import com.lmax.disruptor.dsl.ProducerType;
@@ -24,8 +20,7 @@ public class RingBufferManager {
         LmaxEventFactory eventFactory = new LmaxEventFactory();
         int bufferSize = 1024;
         int threadPoolSize = 200; // Define the size of the thread pool
-        ExecutorService executor = Executors.newFixedThreadPool(threadPoolSize, DaemonThreadFactory.INSTANCE);
-        Disruptor<Event> disruptor = new Disruptor<>(eventFactory, bufferSize, executor, ProducerType.SINGLE, new SleepingWaitStrategy());
+        Disruptor<Event> disruptor = new Disruptor<>(eventFactory, bufferSize, DaemonThreadFactory.INSTANCE, ProducerType.SINGLE, new SleepingWaitStrategy());
         disruptor.handleEventsWith(new LmaxEventHandler());
         RingBuffer<Event> ringBuffer = disruptor.start();
         return ringBuffer;
